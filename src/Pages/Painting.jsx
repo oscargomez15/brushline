c1992839b13ac85768e739eea1115609927b2aa7
Oscar G <murdexchannel@gmail.com>
2025-07-03 01:19:29 -0400

    fixing while in view amount error

--- src/Pages/Painting.jsx
+++ src/Pages/Painting.jsx
@@ -347,7 +347,7 @@ const variants = {
             initial={{ opacity: 0, y: 50 }}
             whileInView={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.6, ease: 'easeOut' }}
-            viewport={{ once: true, amount: 0.5 }}>
+            viewport={{ once: true, amount: 0.2 }}>
 
             <div className="sub-heading">
                 <h1><span>Painting Services</span> we offer</h1>
