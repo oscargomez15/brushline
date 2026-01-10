f02179eebc646ccf0053ea4a2b2369a070bab238
Oscar G <murdexchannel@gmail.com>
2026-01-09 20:11:56 -0500

    redirect to estimator on success login

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,15 +1,39 @@
 import React from 'react'
 import netlifyIdentity from "netlify-identity-widget";
+import { useNavigate } from "react-router-dom";
 import '../Styling/Login.css'
+
 export const Login = () => {
-    const handleLogin = () => {
-    netlifyIdentity.open();
-  };
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    netlifyIdentity.init();
+
+    // If already logged in, go straight to estimator
+    const user = netlifyIdentity.currentUser();
+    if (user) navigate("/estimator");
+
+    const onLogin = () => {
+      netlifyIdentity.close();
+      navigate("/estimator");
+    };
+
+    netlifyIdentity.on("login", onLogin);
+
+    return () => {
+      netlifyIdentity.off("login", onLogin);
+    };
+  }, [navigate]);
+
   return (
-    <div className='login-container page'>
-      <h1>Employee Login</h1>
-      <button onClick={handleLogin}>
-        Login
+    <div className="login-page">
+      <h1>Login</h1>
+      <button
+        type="button"
+        className="collapse-area-btn"
+        onClick={() => netlifyIdentity.open("login")}
+      >
+        Open Login
       </button>
     </div>
   );
